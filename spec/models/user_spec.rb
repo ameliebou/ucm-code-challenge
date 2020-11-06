72dfbd900a7e182f6d198e17bccb42f0d97beef8
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-06 12:25:23 +0100

    wrote tests for User validations on email and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "validates presence of email" do
+    user = User.new
+    user.email = ''
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+
+    user.email = "test@test.com"
+    user.valid?
+    expect(user.errors[:email]).to_not include("can't be blank")
+  end
+
+  it "validates uniqueness of email" do
+    User.create(email: "test@test.com", password: "123456")
+    user = User.new(email: "test@test.com")
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
+  it "validates presence of password" do
+    user = User.new
+    user.password = ""
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+
+    user.password = "123456"
+    user.valid?
+    expect(user.errors[:password]).to_not include("can't be blank")
+  end
 end
