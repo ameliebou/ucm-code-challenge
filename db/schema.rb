7ff457b6f37c0456f58c4150d711804e767268f0
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-09 13:19:25 +0100

    migrated assignments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_112744) do
+ActiveRecord::Schema.define(version: 2020_11_09_120758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assignments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "job_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_assignments_on_job_id"
+    t.index ["user_id"], name: "index_assignments_on_user_id"
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
@@ -38,4 +47,6 @@ ActiveRecord::Schema.define(version: 2020_11_06_112744) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "assignments", "jobs"
+  add_foreign_key "assignments", "users"
 end
