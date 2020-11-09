7a72d0c27d89876136e9e66cfe9d2348105eef05
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-09 14:18:08 +0100

    added user_cannot_apply_twice method

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,12 @@
 class Assignment < ApplicationRecord
   belongs_to :user
   belongs_to :job
+
+  validate :user_cannot_apply_twice
+
+  def user_cannot_apply_twice
+    assignments = Assignment.where(user: user)
+    assignments.select { |assignment| assignment.job == job }
+    errors.add(:user, 'cannot apply twice to the same job') if assignments.length > 0
+  end
 end
