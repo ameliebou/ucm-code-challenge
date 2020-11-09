6c54e3b43612e13532365c14c807e48cd4dd45b8
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-09 14:18:35 +0100

    implemented create action for assignments controller

--- app/controllers/api/v1/assignments_controller.rb
+++ app/controllers/api/v1/assignments_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::AssignmentsController < Api::V1::BaseController
+  before_action :set_job
+  acts_as_token_authentication_handler_for User
+
+  def create
+    @user = current_user
+    @assignment = Assignment.new
+    @assignment.user = @user
+    @assignment.job = @job
+    if @assignment.save
+      render json: @assignment, status: :created
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def set_job
+    @job = Job.find(params[:job_id])
+  end
+
+  def render_error
+    render json: { errors: @assignment.errors.full_messages },
+      status: :unprocessable_entity
+  end
 end
