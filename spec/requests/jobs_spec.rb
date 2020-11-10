e54392368fb78b0fc9828e76c836334285e0ec3f
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-10 13:44:07 +0100

    adapted tests to new jobs index view

--- spec/requests/jobs_spec.rb
+++ spec/requests/jobs_spec.rb
@@ -12,110 +12,35 @@ RSpec.describe "Jobs requests", type: :request do
 
       expect(response).to have_http_status(:success)
       expect(response_body.size).to eq(2)
-      expect(response_body).to eq(
-        [
-          {
-            "title"=> "Host/Hostess",
-            "total_pay"=> 140.0,
-            "spoken_languages"=> [
-                "english",
-                "french"
-            ]
-          },
-          {
-            "title"=> "Kitchen help",
-            "total_pay"=> 112.0,
-            "spoken_languages"=> [
-                "german",
-                "english"
-            ]
-          }
-        ]
-      )
     end
 
     it 'returns a subset of jobs based on pagination' do
       get '/api/v1/jobs', params: { limit: 1 }
 
       expect(response_body.size).to eq(1)
-      expect(response_body).to eq(
-        [
-          {
-            "title"=> "Host/Hostess",
-            "total_pay"=> 140.0,
-            "spoken_languages"=> [
-                "english",
-                "french"
-            ]
-          }
-        ]
-      )
+      expect(response_body.first["title"]).to eq("Host/Hostess")
     end
 
     it 'returns a subset of jobs with an offset' do
       get '/api/v1/jobs', params: { limit: 1, offset: 1 }
 
       expect(response_body.size).to eq(1)
-      expect(response_body).to eq(
-        [
-          {
-            "title"=> "Kitchen help",
-            "total_pay"=> 112.0,
-            "spoken_languages"=> [
-                "german",
-                "english"
-            ]
-          }
-        ]
-      )
+      expect(response_body.first["title"]).to eq("Kitchen help")
     end
 
     it 'searches by title' do
       get '/api/v1/jobs', params: { title: 'kitchen' }
-      expect(response_body).to eq(
-        [
-          {
-            "title"=> "Kitchen help",
-            "total_pay"=> 112.0,
-            "spoken_languages"=> [
-                "german",
-                "english"
-            ]
-          }
-        ]
-      )
+      expect(response_body.first["title"]).to eq("Kitchen help")
     end
 
     it 'searches by spoken language' do
       get '/api/v1/jobs', params: { spoken_language: 'german' }
-      expect(response_body).to eq(
-        [
-          {
-            "title"=> "Kitchen help",
-            "total_pay"=> 112.0,
-            "spoken_languages"=> [
-                "german",
-                "english"
-            ]
-          }
-        ]
-      )
+      expect(response_body.first["title"]).to eq("Kitchen help")
     end
 
     it 'searches by title and spoken language' do
       get '/api/v1/jobs', params: { spoken_language: 'english', title: 'host' }
-      expect(response_body).to eq(
-        [
-          {
-            "title"=> "Host/Hostess",
-            "total_pay"=> 140.0,
-            "spoken_languages"=> [
-                "english",
-                "french"
-            ]
-          }
-        ]
-      )
+      expect(response_body.first["title"]).to eq("Host/Hostess")
     end
   end
 
