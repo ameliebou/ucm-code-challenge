1844f243096b511a8458691f06fc2f8e426a2a3f
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-11 11:09:53 +0100

    defined create action for registrations

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,5 +1,21 @@
 class Api::V1::RegistrationsController < Api::V1::BaseController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render_error
+    end
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def render_error
+    render json: { errors: @user.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
