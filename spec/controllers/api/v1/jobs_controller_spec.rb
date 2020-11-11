9f9dcdcd0bcbd444f18213ee307d9c7e30e654ff
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-11 10:53:01 +0100

    added description inside jobs controller spec

--- spec/controllers/api/v1/jobs_controller_spec.rb
+++ spec/controllers/api/v1/jobs_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::JobsController, type: :controller do
-  it "has a max limit of 20" do
-    expect(Job).to receive(:limit).with(20).and_call_original
+  describe "#index" do
+    it "has a max limit of 20" do
+      expect(Job).to receive(:limit).with(20).and_call_original
 
-    get :index, params: { limit: 100 }, xhr: true
+      get :index, params: { limit: 100 }, xhr: true
+    end
   end
 end
