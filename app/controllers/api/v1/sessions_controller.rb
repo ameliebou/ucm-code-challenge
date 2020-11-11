315ef5791b3ada57ad771185c35472648bd59192
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-11 11:42:48 +0100

    defined create action on sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,37 @@
 class Api::V1::SessionsController < Api::V1::BaseController
+  before_action :sign_in_params, only: :create
+  before_action :load_user, only: :create
+
+  def create
+    if @user.valid_password?(sign_in_params[:password])
+      sign_in "user", @user
+      render json: {
+        messages: "Signed In Successfully",
+        is_success: true,
+        data: {user: @user}
+      }, status: :ok
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def sign_in_params
+    params.require(:user).permit :email, :password
+  end
+
+  def load_user
+    @user = User.find_for_database_authentication(email: sign_in_params[:email])
+    if @user
+      return @user
+    else
+      render_error
+    end
+  end
+
+  def render_error
+    render json: { errors: @user.errors.full_messages },
+      status: :failure
+  end
 end
