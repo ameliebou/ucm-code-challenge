18c2650bd6393f578d9fa5b5e146bd2a32941243
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-11 11:47:26 +0100

    added tests for sessions controller

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -12,5 +12,16 @@ RSpec.describe Api::V1::SessionsController, type: :controller do
         }, xhr: true
       expect(JSON.parse(response.body)["messages"]).to eq("Signed In Successfully")
     end
+
+    it "fails if user provides the wrong password" do
+      User.create(email: "test@test.com", password: "123456")
+      post :create, params: {
+        "user": {
+          "email":"test@test.com",
+          "password":"somethingelse",
+         }
+        }, xhr: true
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
   end
 end
