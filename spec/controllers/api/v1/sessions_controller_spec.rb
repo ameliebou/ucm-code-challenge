879182ea1cd6bc9983d7a2174c237b713d9c99c8
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-11 11:42:12 +0100

    improved test on sessions controller

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Api::V1::SessionsController, type: :controller do
           "password":"123456",
          }
         }, xhr: true
-      puts current_user
+      expect(JSON.parse(response.body)["messages"]).to eq("Signed In Successfully")
     end
   end
 end
