bbe8e444207808d69b4fbced554eaa093b15923d
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-11 10:49:23 +0100

    added sign up and sign in routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
+      end
       resources :jobs, only: [ :index, :create ]
       resources :jobs, only: [ :show ] do
         resources :assignments, only: [ :create ]
