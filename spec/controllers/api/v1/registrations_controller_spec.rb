1a8c6e66aaa1ccf696539cc5198ab6ba796ff53b
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-11 11:05:20 +0100

    improved registrations controller spec

--- spec/controllers/api/v1/registrations_controller_spec.rb
+++ spec/controllers/api/v1/registrations_controller_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 RSpec.describe Api::V1::RegistrationsController, type: :controller do
   describe "#create" do
     it "should create a user" do
-      get :create, params: {
+      users = User.count
+      post :create, params: {
         "user": {
           "email":"abc@example.com",
           "password":"password",
           "password_confirmation":"password"
          }
         }, xhr: true
+      expect(User.count).to eq(users + 1)
     end
   end
 end
