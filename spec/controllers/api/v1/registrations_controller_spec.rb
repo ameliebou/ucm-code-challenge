a3f4931d518431df0578c76b6acddca3f0d7c060
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-11 11:50:16 +0100

    added tests for registrations controller

--- spec/controllers/api/v1/registrations_controller_spec.rb
+++ spec/controllers/api/v1/registrations_controller_spec.rb
@@ -13,5 +13,16 @@ RSpec.describe Api::V1::RegistrationsController, type: :controller do
         }, xhr: true
       expect(User.count).to eq(users + 1)
     end
+
+    it "shouldn't allow to create a user if the password_confirmation is wrong" do
+      post :create, params: {
+        "user": {
+          "email":"abc@example.com",
+          "password":"password",
+          "password_confirmation":"otherpassword"
+         }
+        }, xhr: true
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
   end
 end
