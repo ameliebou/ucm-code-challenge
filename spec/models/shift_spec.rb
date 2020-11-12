8045a7b5dc97ecddaa6ef30f71efe437645aed0a
Amelie Boudier <amelie.boudier01@gmail.com>
2020-11-12 11:32:55 +0100

    added test for shift validation

--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
+require 'date'
 
 RSpec.describe Shift, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "must have a start and an end" do
+    shift = Shift.new
+    shift.valid?
+    expect(shift.errors[:start]).to include("can't be blank")
+    expect(shift.errors[:end]).to include("can't be blank")
+
+    shift.start = DateTime.new(2020,12,01,11)
+    shift.end = DateTime.new(2020,12,01,13)
+    shift.valid?
+    expect(shift.errors[:start]).to_not include("can't be blank")
+    expect(shift.errors[:end]).to_not include("can't be blank")
+  end
 end
